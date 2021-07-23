f015fd22caf3fbcc60068e5c703bec54c6b70c74
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-07-23 18:14:36 +0200

    Base commit

---
+++ src/main/java/io/qiot/manufacturing/commons/util/producer/LoggerProducer.java
@@ -0,0 +1,20 @@
+package io.qiot.manufacturing.commons.util.producer;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@ApplicationScoped
+public class LoggerProducer {
+
+    @Produces
+    public Logger getLogger(final InjectionPoint ip) {
+        return LoggerFactory
+                .getLogger(ip.getMember().getDeclaringClass());
+    }
+}
