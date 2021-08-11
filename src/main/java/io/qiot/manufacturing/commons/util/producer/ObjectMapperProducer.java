b65e38c30138ea953a522ad2332460b0921e90c5
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-08-11 20:26:51 +0200

    alpha4

--- src/main/java/io/qiot/manufacturing/commons/util/producer/ObjectMapperProducer.java
+++ src/main/java/io/qiot/manufacturing/commons/util/producer/ObjectMapperProducer.java
@@ -5,6 +5,7 @@ import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
  * @author andreabattaglia
@@ -17,11 +18,12 @@ public class ObjectMapperProducer {
 
     public ObjectMapperProducer() {
         MAPPER = new ObjectMapper();
-//        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
-//                .allowIfBaseType(AbstractTelemetry.class).build();
-//        MAPPER.activateDefaultTyping(ptv,
-//                ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE);
-//        MAPPER.registerModule(new JavaTimeModule());
+        // PolymorphicTypeValidator ptv =
+        // BasicPolymorphicTypeValidator.builder()
+        // .allowIfBaseType(AbstractTelemetry.class).build();
+        // MAPPER.activateDefaultTyping(ptv,
+        // ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE);
+        MAPPER.registerModule(new JavaTimeModule());
     }
 
     @Produces
